5e5a6a0323d091cba80bcedc69af63e771e09bee
Yaru Wang <yaruwang.materials@gmail.com>
2026-06-07 23:28:24 +0200

    remove contrib

--- src/struct_extract_eval/contrib/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Domain-specific comparators and utilities.
-
-Each subpackage provides comparators tailored to a specific domain.
-Install the corresponding optional extra to get the dependencies:
-
-    pip install struct-extract-eval[materials]
-"""
